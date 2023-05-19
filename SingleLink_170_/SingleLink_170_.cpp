b7ac58ecb5a40ce39a68d3ae61d89a14a364d2c7
kaniadhea <kaniadhea7@gmail.com>
2023-05-19 02:19:00 -0700

    break

--- SingleLink_170_/SingleLink_170_.cpp
+++ SingleLink_170_/SingleLink_170_.cpp
@@ -136,6 +136,22 @@ int main() {
 	{
 		try
 		{
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapuus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan);
+			{
+			case 1:
+				addNode();
+				coout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			}
 
 		}
 	}
