a866c6e5dc17bba9a7fb6035271d8235258a4bd3
kaniadhea <kaniadhea7@gmail.com>
2023-05-19 01:14:21 -0700

    start

---
+++ SingleLink_170_/SingleLink_170_.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int noMhs;
+	string name;
+	Node* next;
+};
+
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIm: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if ()
+	}
+}
